c479902786299efefcb551a3aa825af4e372da5f
Zion <omogbemezion@gmail.com>
2025-02-01 23:15:09 +0100

    finished

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,26 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";  // Import CORS
 import evotlRouter from "./routes/evotlRoutes";
-// import bodyParser from "body-parser"
 
 dotenv.config();
 const app = express();
 
 // Enable CORS for all origins (for development purposes)
-app.use(cors());
-
-// If you want to restrict it to a specific origin (like your frontend)
-// Configure the CORS middleware with specific options
-app.use(cors({
+const corsOptions = {
   origin: 'http://localhost:5173',  // Replace with your frontend URL if needed
-  methods: ['GET', 'POST'],  // Allow specific HTTP methods
-  allowedHeaders: ['Content-Type'],  // Allow specific headers
-}));
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allow methods as needed
+  allowedHeaders: ['Content-Type'],  // Allow headers as needed
+};
+
+app.use(cors(corsOptions));  // Apply CORS globally
 
 app.use(express.json());
-// app.use(bodyParser.json());
 app.use("/api/evtols", evotlRouter);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`🚀 Server is running on port ${PORT}`);
+  console.log(`🚀 Server is running on port ${PORT}`);
 });
+
 
